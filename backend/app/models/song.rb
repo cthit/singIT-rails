e27335cc3cac9c2dbb713481b9b42fe7dbc26618
Jakob Holmgren <jholmgren95@gmail.com>
2016-08-29 16:06:19 +0200

    Annotated stuff

--- backend/app/models/song.rb
+++ backend/app/models/song.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: songs
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  artist     :string
+#  imageUrl   :string
+#  mp3hash    :string
+#  hash       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Song < ApplicationRecord
 end

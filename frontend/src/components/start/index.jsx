ca76106b459edc53ebf6da528de480612f41d330
juice <juice@chalmers.it>
2016-10-30 20:27:12 +0100

    sticky through scrollable div + sorting button

--- frontend/src/components/start/index.jsx
+++ frontend/src/components/start/index.jsx
@@ -2,9 +2,9 @@
 
 import React, {PropTypes} from 'react';
 import Infinite from 'react-infinite';
-import { StickyContainer, Sticky } from 'react-sticky';
 import Fuse from 'fuse.js';
 import _ from 'lodash';
+import moment from 'moment';
 import {fetchJson} from '../../services/backend';
 
 import styles from './style.css'
@@ -14,7 +14,8 @@ const Start = React.createClass({
       return {
         searchString: '',
         songs: [],
-        filteredSongs: []
+        filteredSongs: [],
+        sortingMethod: this.artistSort
       };
     },
 
@@ -23,17 +24,20 @@ const Start = React.createClass({
       fetchJson('/api/songs.json').then(songs => {
         const sortedSongs = songs
         .filter(s => s.title && s.title.length > 0 && s.artist && s.artist.length > 0)
-        .sort((a, b) => {
-          const artistDiff = a.artist.toLowerCase().localeCompare(b.artist.toLowerCase());
-          return artistDiff == 0
-            ? a.title.toLowerCase().localeCompare(b.title.toLowerCase())
-            : artistDiff;
-        });
+        .sort(this.artistSort);
         this.setState({
           songs: sortedSongs,
-          filteredSongs: sortedSongs
+          filteredSongs: sortedSongs,
+          sortingMethod: this.artistSort
         })
       });
+    },
+
+    renderDate(date) {
+      if ((new Date(date)).getYear() === (new Date()).getYear())
+        return <div className={styles.date}>{moment(date).format("D/M")}</div>
+      else
+        return <div className={styles.date}>{moment(date).format("YYYY")}</div>
     },
 
     renderSong(song) {
@@ -45,6 +49,7 @@ const Start = React.createClass({
             <div className={styles.title}>{song.title}</div>
             <div className={styles.artist}>{song.artist}</div>
           </div>
+          {this.renderDate(song.created_at)}
         </div>
       )
     },
@@ -68,7 +73,6 @@ const Start = React.createClass({
             ]
       };
 
-
       if (searchString.length <= 1) {
         this.setState({ filteredSongs: songs });
         return;
@@ -81,13 +85,41 @@ const Start = React.createClass({
       })
     },
 
+    artistSort(a, b) {
+      const artistDiff = a.artist.toLowerCase().localeCompare(b.artist.toLowerCase());
+      return artistDiff == 0
+        ? a.title.toLowerCase().localeCompare(b.title.toLowerCase())
+        : artistDiff;
+    },
+
+    timeSort(a, b) {
+      return a.created_at == b.created_at ? 0 : a.created_at < b.created_at ? 1 : -1;
+    },
+
+    sortSongs(sortingMethod) {
+      const songs = this.state.songs.sort(sortingMethod);
+      const filteredSongs = this.state.filteredSongs.sort(sortingMethod);
+      this.setState({songs, filteredSongs, sortingMethod})
+    },
+
+    renderSortButton() {
+      switch (this.state.sortingMethod) {
+        case this.artistSort:
+          return <i className="fa fa-clock-o" onClick={() => this.sortSongs(this.timeSort)}></i>
+        case this.timeSort:
+          return <i className="fa fa-sort-alpha-asc" onClick={() => this.sortSongs(this.artistSort)}></i>
+        default:
+          return <i className="fa fa-clock-o" onClick={() => this.sortSongs(this.timeSort)}></i>
+        }
+    },
+
     render() {
         const { filteredSongs, searchString } = this.state;
 
         return (
-            <StickyContainer className={styles.container}>
-              <h1>singIT<span className={styles.version}>beta</span></h1>
-              <Sticky>
+          <div className={styles.container}>
+            <div className={styles.sticky}>
+              <div className={styles.searchAndSort}>
                 <input type="text"
                        autoFocus={true}
                        className={styles.searchBox}
@@ -95,12 +127,14 @@ const Start = React.createClass({
                        onChange={this.handleSearchInput}
                        value={searchString}
                        placeholder="Search" />
-              </Sticky>
+                 {this.renderSortButton()}
+               </div>
               <div className={styles.hits}>Hits: {filteredSongs.length}</div>
-              <Infinite useWindowAsScrollContainer elementHeight={50}>
-                {filteredSongs.map(s => this.renderSong(s))}
-              </Infinite>
-            </StickyContainer>
+            </div>
+            <Infinite useWindowAsScrollContainer elementHeight={50} className={styles.songList}>
+              {filteredSongs.map(s => this.renderSong(s))}
+            </Infinite>
+          </div>
         )
     }
 });
